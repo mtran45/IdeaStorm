c272f44f03eb18e518431659328cdd9662cafe94
Michael Tran <mtran45@gmail.com>
2017-01-22 00:20:28 +1100

    Add basic list view

--- IdeaStorm.WebUI/App_Start/RouteConfig.cs
+++ IdeaStorm.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace IdeaStorm.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Idea", action = "List", id = UrlParameter.Optional }
             );
         }
     }
---
+++ IdeaStorm.WebUI/Controllers/IdeaController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IdeaStorm.Domain.Abstract;
+
+namespace IdeaStorm.WebUI.Controllers
+{
+    public class IdeaController : Controller
+    {
+        private IIdeaRepository repository;
+
+        public IdeaController(IIdeaRepository ideaRepository)
+        {
+            this.repository = ideaRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Ideas);
+        }
+    }
+}
